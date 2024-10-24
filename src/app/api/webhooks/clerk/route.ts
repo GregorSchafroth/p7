3963df42d05cf795359bf47446038bce73e89272
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-10-24 08:52:05 +0200

    add webhook

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,62 @@
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { env } from '@/data/env/server'
+import { createUserSubscription } from '@/server/db/subscriptions'
+import { deleteUser } from '@/server/db/users'
+
+export async function POST(req: Request) {
+  // Get the headers
+  const headerPayload = headers()
+  const svixId = headerPayload.get('svix-id')
+  const svixTimestamp = headerPayload.get('svix-timestamp')
+  const svixSignature = headerPayload.get('svix-signature')
+
+  // If there are no headers, error out
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400,
+    })
+  }
+
+  // Get the body
+  const payload = await req.json()
+  const body = JSON.stringify(payload)
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(env.CLERK_WEBHOOK_SECRET)
+
+  let event: WebhookEvent
+
+  // Verify the payload with the headers
+  try {
+    event = wh.verify(body, {
+      'svix-id': svixId,
+      'svix-timestamp': svixTimestamp,
+      'svix-signature': svixSignature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error occured', {
+      status: 400,
+    })
+  }
+
+  switch (event.type) {
+    case 'user.created': {
+      await createUserSubscription({
+        clerkUserId: event.data.id,
+        tier: 'Free',
+      })
+      break
+    }
+    case 'user.deleted': {
+      if (event.data.id != null) {
+        await deleteUser(event.data.id)
+        // TODO remove stripe subscritpion
+      }
+    }
+  }
+
+  return new Response('', { status: 200 })
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import { auth } from '@clerk/nextjs/server'
+
+export default async function DashboardPage() {
+  const { userId, redirectToSignIn } = await auth()
+  if (userId === null) redirectToSignIn()
+
+  const products = await getProducts(userId)
+  return null
 }
-export default page
